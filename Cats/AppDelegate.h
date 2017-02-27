2a5d4c7a7332cbd7907ed30bd60dbc609aa6039a
daveaugerinos <dave.augerinos@gmail.com>
2017-02-27 13:42:30 -0800

    Initial setup

---
+++ Cats/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cats
+//
+//  Created by Dave Augerinos on 2017-02-27.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
